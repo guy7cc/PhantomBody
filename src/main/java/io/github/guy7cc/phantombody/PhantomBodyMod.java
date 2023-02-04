0917be10634c8c941995cde85c3e43e6acbe99b8
guy7cc <alrdy.tree1sawa@gmail.com>
2023-02-05 01:51:45 +0900

    Initial commit

---
+++ src/main/java/io/github/guy7cc/phantombody/PhantomBodyMod.java
@@ -0,0 +1,8 @@
+package io.github.guy7cc.phantombody;
+
+import net.minecraftforge.fml.common.Mod;
+
+@Mod(PhantomBodyMod.ID)
+public class PhantomBodyMod {
+    public static final String ID = "phantombody";
+}
